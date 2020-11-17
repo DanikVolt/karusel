671d18d7d14373d6cdb4b978a3bd43ab4205653e
Opilane TTHK <opilane@tthk.ee>
2020-11-17 12:27:37 +0200

    Add project files.

---
+++ karusel/karusel/MainPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace karusel
+{
+    public partial class MainPage : CarouselPage
+    {
+        public MainPage()
+        {
+            var RedPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                    new Label
+                    {
+                        Text = "Red",
+                        FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label)),
+                        HorizontalOptions = LayoutOptions.Center
+                    },
+                    new Image
+                    {
+
+                    }
+                    }
+                }
+            };
+            var GreenPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                    new Label
+                    {
+                        Text = "Greed",
+                        FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label)),
+                        HorizontalOptions = LayoutOptions.Center
+                    },
+                    new BoxView
+                    {
+                        Color = Color.Green,
+                        WidthRequest = 200,
+                        HeightRequest = 200,
+                        HorizontalOptions = LayoutOptions.Center,
+                        VerticalOptions = LayoutOptions.CenterAndExpand
+                    }
+                    }
+                }
+            };
+            var BluePage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                    new Label
+                    {
+                        Text = "Blue",
+                        FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label)),
+                        HorizontalOptions = LayoutOptions.Center
+                    },
+                    new BoxView
+                    {
+                        Color = Color.Blue,
+                        WidthRequest = 200,
+                        HeightRequest = 200,
+                        HorizontalOptions = LayoutOptions.Center,
+                        VerticalOptions = LayoutOptions.CenterAndExpand
+                    }
+                    }
+                }
+            };
+            Children.Add(RedPage);
+            Children.Add(GreenPage);
+            Children.Add(BluePage);
+            Chil
+        }
+    }
+}
